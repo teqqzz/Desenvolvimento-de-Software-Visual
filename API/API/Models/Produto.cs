0f19e479ff8606240e2f8249847bc6a45cb7c814
Lucas de Oliveira Rosa <lucastequizu@gmail.com>
2024-10-21 12:28:06 -0300

    Alterado a forma de como e cadastro o id

--- API/API/Models/Produto.cs
+++ API/API/Models/Produto.cs
@@ -2,19 +2,12 @@ namespace API.Models
 {
     public class Produto
     {   
-
-        //C# Contrutor
-        public Produto(){
-            Id = Guid.NewGuid().ToString();
-            CriadoEm = DateTime.Now;
-        }
-
         //Atributos/Propriedades - Caracteristicas
         public string? Id {get; set;}
         public string? Nome {get; set;}
         public double Valor {get; set;}
         public int Quantidade { get; set;}
-        public DateTime CriadoEm{get; set;}
+        public DateTime CriadoEm {get; set;} = DateTime.Now;
 
     }
 }
